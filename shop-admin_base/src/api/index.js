a1cffdfbcdfca7ce9db30ba1b08a35c2ee4ac1e1
enheng1238 <223547544@qq.com>
2020-09-22 19:54:35 +0800

    2.导入和暴露

--- shop-admin_base/src/api/index.js
+++ shop-admin_base/src/api/index.js
@@ -6,6 +6,80 @@ export {default as category} from './category'
 export {default as clientUser} from './clientUser'
 export {default as order} from './order'
 
+/**
+ * 1.
+ * export {default as order} from './order'
+ * 理解：
+ * import {default as order} from './order'
+ * export order
+ * 
+ * 2.
+ * export {default as clientUser} from './clientUser'
+ * 理解:
+ * import {default as clientUser} from './clientUser'
+ * export clientUser
+ * 
+ * 最终分别暴露的两个对象
+ * order = {
+ * 
+ * //获取订单分页列表(带搜索)
+ * 
+ * getPageList(page, limit, searchObj) {
+ *  return request({
+ *    url: `${api_name}/${page}/${limit}`,
+ *    method: 'get',
+ *     params: searchObj // url查询字符串或表单键值对
+ *  })
+ * }
+ * }
+ * 
+ * clientUser = {
+ *
+ * 获取会员分页列表(带搜索)
+ *
+ * getPageList(page, limit, searchParams) {
+ *  
+ *  return request({
+ *     url: `/admin/user/${page}/${limit}`,
+ *     method: 'GET',
+ *    params: searchParams
+ *  })
+ * }
+ *}
+ * 
+ * 
+ * main.js
+ * 引入到同一个文件然后一起暴露打包为一个对象,不同文件中无法做到
+ * import * as API from '@/api'
+ * 
+ * 挂载到Vue原型对象上, 以便组件中直接可见
+ * Vue.prototype.$API = API
+ * 
+ * API = {
+ *    order,
+ *    clientUser
+ * }
+ * 
+ * 
+ * API.clientUser.getPageList()
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /* {
