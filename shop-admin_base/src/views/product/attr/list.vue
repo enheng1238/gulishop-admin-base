5ec654e0ac97550ed4e4c2ae5be473c4bd1518d2
enheng1238 <223547544@qq.com>
2020-09-24 20:25:08 +0800

    22.card --- 3D

--- shop-admin_base/src/views/product/attr/list.vue
+++ shop-admin_base/src/views/product/attr/list.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>attrList</div>
+  <div>
+    <el-card>
+      <!-- 三级分类的组件 -->
+      三级分类
+    </el-card>
+
+    <el-card style="margin-top:20px">
+      <!-- 列表数据展示的内容 -->
+      列表数据数据
+    </el-card>
+
+  </div>
 </template>
 
 <script>
