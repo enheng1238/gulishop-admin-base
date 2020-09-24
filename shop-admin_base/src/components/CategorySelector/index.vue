5cd4c4d7a9610bc2c2ecf1dbcb456f6b12bdc0a6
enheng1238 <223547544@qq.com>
2020-09-24 21:29:36 +0800

    28.二三级列表数据

--- shop-admin_base/src/components/CategorySelector/index.vue
+++ shop-admin_base/src/components/CategorySelector/index.vue
@@ -2,7 +2,7 @@
   <el-form :inline="true" :model="cform">
   <!-- form 一定要指定一个对象model   用来收集数据 -->
     <el-form-item label="一级分类">
-      <el-select v-model="cform.category1Id" placeholder="请选择">
+      <el-select v-model="cform.category1Id" placeholder="请选择" @change="handlerCategory1">
         <!-- 
           select 收集的数据是选中的那个value值
           select本身并没有value值 它的value值是他选中的option的value值
@@ -13,6 +13,11 @@
           一级分类一上来就有数据,因此在mounted中发请求
           选中一级的时候二级才有数据
           选中二级的时候三级才有数据
+
+         select  事件
+          change 选中值发生变化时触发  回调函数:你选中的值
+          @ 事件
+          : 属性
          -->
         <el-option :label="c1.name" :value="c1.id" v-for="(c1,index) in category1List" :key="c1.id"></el-option>
         
@@ -20,16 +25,15 @@
     </el-form-item>
     
       <el-form-item label="二级分类">
-      <el-select v-model="cform.category2Id" placeholder="请选择">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
+      <el-select v-model="cform.category2Id" placeholder="请选择" @change="handlerCategory2">
+         <el-option :label="c2.name" :value="c2.id" v-for="(c2,index) in category2List" :key="c2.id"></el-option>
+        
       </el-select>
     </el-form-item>
 
       <el-form-item label="三级分类">
-      <el-select v-model="cform.category3Id" placeholder="请选择">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
+      <el-select v-model="cform.category3Id" placeholder="请选择" @change="handlerCategory3">
+         <el-option :label="c3.name" :value="c3.id" v-for="(c3,index) in category2List" :key="c3.id"></el-option>
       </el-select>
     </el-form-item>
   </el-form>
@@ -56,13 +60,32 @@ export default {
     this.getCategoryList()
   },
   methods:{
+    // 获取一级分类列表
     async getCategoryList(){
       const result = await this.$API.category.getCategorys1()
 
       if(result.code === 200){
         this.category1List = result.data
       }
+    },
+
+    // 选中1级分类的时候获取二级分类列表
+    async handlerCategory1(category1Id){
+      const result = await this.$API.category.getCategorys2(category1Id)
+
+      if(result.code === 200){
+        this.category2List = result.data
+      }
+    },
+
+    // 选中二级分类的时候获取三级分类列表
+    async handlerCategory2(category2Id){
+      const result = await this.$API.category.getCategorys3(category2Id)
+      if(result.code === 200){
+        this.category3List = result.data
+      }
     }
+  
   }
 };
 </script>
