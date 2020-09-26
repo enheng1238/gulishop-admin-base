050bb755d69c513741e163d391715943443affb9
enheng1238 <223547544@qq.com>
2020-09-26 09:50:19 +0800

    1.收集属性值

--- shop-admin_base/src/views/product/attr/list.vue
+++ shop-admin_base/src/views/product/attr/list.vue
@@ -6,6 +6,7 @@
       <CategorySelector @handlerCategory="handlerCategory"></CategorySelector>
     </el-card>
 
+<!-- 静态页面是为了收集数据的 -->
     <el-card style="margin-top: 20px">
       <div v-show="isShowList">
         <!-- 列表数据展示的内容 -->
@@ -58,15 +59,16 @@
       <div v-show="!isShowList">
         <!-- 添加和修改的页面 -->
         <!-- :inline="true" 行内form -->
-        <el-form :inline="true">
-          <el-form-item label="属性名">
-            <el-input placeholder="请输入属性名"></el-input>
+        <el-form :inline="true" :model="attr">
+          <el-form-item label="属性名" >
+            <el-input placeholder="请输入属性名" v-model="attr.attrName" ></el-input>
           </el-form-item>
         </el-form>
 
-        <el-button type="primary" icon="el-icon-plus">添加属性值</el-button>
+        <el-button type="primary" icon="el-icon-plus" @click="addAttrValue">添加属性值</el-button>
         <el-button >取消</el-button>
         <el-table
+          :data="attr.attrValueList"
           border
           style="width: 100%;margin:20px 0">
           <el-table-column
@@ -78,6 +80,11 @@
             prop="prop"
             label="属性值名称"
             width="width">
+            <!-- :data="attr.attrValueList"  row代表的是某一个属性值 -->
+            <template slot-scope="{row,$index}">
+              <el-input v-model="row.valueName" placeholder="请输入属性值"></el-input>
+            </template>
+           
           </el-table-column>
             <el-table-column
             label="操作"
@@ -103,10 +110,45 @@ export default {
       category2Id: "",
       category3Id: "",
       attrList: [],
-      isShowList:false
+      isShowList:false,
+
+      // 收集的数据
+      attr:{
+        attrName:"",
+        attrValueList:[],
+        categoryId:0,
+        categoryLevel:3,
+      },
+
+
+/*
+      {
+        "attrName": "string",
+        "attrValueList": [
+          {
+            "attrId": 0,
+            "id": 0,
+            "valueName": "string"
+          }
+        ],
+        "categoryId": 0,
+        "categoryLevel": 0,
+        "id": 0
+    }
+*/
+
     };
   },
   methods: {
+    // addAttrValue 点击添加属性值的回调
+    // 属性值的对象  收集属性值的时候 我们的做法是先在属性值列表当中添加一个属性的对象， 然后上面table就会出现一行数据，只不过都是空的
+    addAttrValue(){
+      this.attr.attrValueList.push({
+         attrId:this.attr.id || undefined,//属性值所属属性的id
+        //  id: 0,//新添加的没有id
+         valueName: ""//属性值已经添加到属性值列表当中了，但是这个属性值的名称是空串是为了等待用户去输入输入
+      })
+    },
     // 自定义事件 回调函数留在父中
     handlerCategory({ categoryId, level }) {
       if (level === 1) {
